740874b37102592549f24a92fa78fd963658ce3a
chris23rocha <chris23rocha@gmail.com>
2022-07-23 21:55:38 -0500

    more updates to .JS

--- script.js
+++ script.js
@@ -20,6 +20,33 @@ if (passwordLength < 8 || passwordLength > 128) {
 alert ("password length must be between 8 and 128. Please try again.");
 generatePassword ();
 }
+
+includelc = confirm ("Press ok to include lowercase characters in password.");
+includeuc = confirm ("Press ok to include uppercase characters in password.");
+includenum = confirm ("Press ok to include numbers in password.");
+includesymbols = confirm ("Press ok to include symbols in password.");
+
+if (includelc === false && includeuc === false&& includenum === false&& includesymbols ===false) {
+  alert ("Password should include at least one character type. Please include.");
+  generatePassword ();
+}
+
+if (includelc){
+  possChars = possChars + lcChars;
+  console.log ("possChars = ", possChars);
+}
+if (includeuc){
+  possChars = possChars + ucChars;
+  console.log ("possChars = ", possChars);
+}
+if (includenum){
+  possChars = possChars + num;
+  console.log ("possChars = ", possChars);
+}
+if (includesymbols){
+  possChars = possChars + symbols;
+  console.log ("possChars = ", possChars);
+}
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
