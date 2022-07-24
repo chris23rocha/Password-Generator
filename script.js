815e8e17117ab41894fe0480651f8ac460da925b
chris23rocha <chris23rocha@gmail.com>
2022-07-23 21:42:25 -0500

    updated scipt.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment code here
 
+function generatePassword() {
+let lcChars = "abcdefghijklmnopqrstuwxyz";
+let ucChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let num = "0123456789";
+let symbols = "*!$#%^";
+let possChars = "";
+let password = "";
+let includelc = false;
+let includeuc = false;
+let includenum = false;
+let includesymbols = false;
+let passwordLength = 0;
 
+passwordLength = parseInt(prompt("provide password length between 8 and 128"));
+console.log ("passwordLength = ", passwordLength);
+
+if (passwordLength < 8 || passwordLength > 128) { 
+alert ("password length must be between 8 and 128. Please try again.");
+generatePassword ();
+}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
